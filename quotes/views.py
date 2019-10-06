6eeae4719ba4fbc26bc8c91f06fef27460899d03
Heather Condron <41925920+hicondron@users.noreply.github.com>
2019-10-06 14:39:31 -0400

    add API

--- quotes/views.py
+++ quotes/views.py
@@ -3,7 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+
+    import requests
+    import json
+
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_52ea6173dbb24a05a1a9b25eb6e52c41")
+
+    try: 
+        api = json.loads(api_request.content)
+
+    except Exception as e:
+        api = "Error ...."
+        
+    return render(request, 'home.html', {'api':api})
+    # pk_52ea6173dbb24a05a1a9b25eb6e52c41
 
 def about(request):
     return render(request, 'about.html', {})
