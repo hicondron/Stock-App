4f77209a1b6efbc4d9139db2d0adaa1b865b8a3a
Heather Condron <41925920+hicondron@users.noreply.github.com>
2019-10-06 21:56:01 -0400

    corrected views page error with else

--- quotes/views.py
+++ quotes/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 
 def home(request):
-    import requests
-    import json
-    if request.method == 'POST':
-        ticker = request.POST['ticker']
-        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_52ea6173dbb24a05a1a9b25eb6e52c41")
-        try:
-            api = json.loads(api_request.content)
-        except Exception as e:
-            api = "Error...."
-    else
-        return render(request, 'home.html', {'ticker': "Enter your ticker symbol here"})
-            
+	import requests 
+	import json 
+
+	if request.method == 'POST':
+		ticker = request.POST['ticker']
+		api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_52ea6173dbb24a05a1a9b25eb6e52c41")
+
+		try:
+			api = json.loads(api_request.content)
+		except Exception as e:
+			api = "Error..."
+		return render(request, 'home.html', {'api': api})
+
+	else:
+		return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})       
             
 def about(request):
         return render(request, 'about.html', {})
