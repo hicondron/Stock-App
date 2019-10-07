7a08e538a212afe3c59e89df972295c78c339507
Heather Condron <41925920+hicondron@users.noreply.github.com>
2019-10-06 21:05:43 -0400

    add if/else statement

--- quotes/views.py
+++ quotes/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def home(request):
-
     import requests
     import json
-
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_52ea6173dbb24a05a1a9b25eb6e52c41")
-
-    try: 
-        api = json.loads(api_request.content)
-
-    except Exception as e:
-        api = "Error ...."
-        
-    return render(request, 'home.html', {'api':api})
-    # pk_52ea6173dbb24a05a1a9b25eb6e52c41
-
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_52ea6173dbb24a05a1a9b25eb6e52c41")
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error...."
+    else
+        return render(request, 'home.html', {'ticker': "Enter your ticker symbol here"})
+            
+            
 def about(request):
-    return render(request, 'about.html', {})
+        return render(request, 'about.html', {})
