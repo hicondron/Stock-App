cca6ba7af8d3d754e8436253133e930f1e8c3ce6
Heather Condron <41925920+hicondron@users.noreply.github.com>
2019-10-11 20:52:45 -0400

    connect API to database

--- quotes/views.py
+++ quotes/views.py
@@ -3,7 +3,6 @@ from .models import Stock
 from .forms import StockForm
 from django.contrib import messages
 
-
 def home(request):
 	import requests 
 	import json 
@@ -21,28 +20,46 @@ def home(request):
 	else:
 		return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
 
+
+
+	
+
+
+	
+
 def about(request):
 	return render(request, 'about.html', {})
 
+
 def add_stock(request):
-	import requests
-	import json
+	import requests 
+	import json 
 
 	if request.method == 'POST':
 		form = StockForm(request.POST or None)
 
 		if form.is_valid():
 			form.save()
-			messages.success(request, ("Stock has been added"))
+			messages.success(request, ("Stock Has Been Added!"))
 			return redirect('add_stock')
-	
-	else:
+
+	else:	
 		ticker = Stock.objects.all()
 		output = []
-		return render(request, 'add_stock.html', {'ticker': ticker})
+		for ticker_item in ticker:
+			api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_062031d20883444f9ea74e2610fe2011")
+			try:
+				api = json.loads(api_request.content)
+				output.append(api)
+			except Exception as e:
+				api = "Error..."
+		
+		return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
+
 
 def delete(request, stock_id):
 	item = Stock.objects.get(pk=stock_id)
 	item.delete()
-	messages.success(request, ("Stock has been deleted from your portfolio"))
-	return redirect('add_stock')
+	messages.success(request, ("Stock Has Been Deleted!"))
+	return redirect(delete_stock)
+
