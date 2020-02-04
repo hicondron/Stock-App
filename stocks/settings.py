dad2eed3f61d212e031d53175b0bca90563278c3
EC2 Default User <ec2-user@ip-172-31-86-21.ec2.internal>
2020-02-04 02:06:46 +0000

    add styling to about page

--- stocks/settings.py
+++ stocks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0w4@41&=)d&ca20!&1x6iylo%sw9%@g@7h$6xjiii4b^@uv)g*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'stocks.urls'
